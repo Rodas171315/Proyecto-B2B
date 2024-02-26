acf28513457f08a146d1dff0c9897881b439f847
Rodas171315 <rodas171315@unis.edu.gt>
2024-02-26 00:44:30 +0000

    Eliminando archivos

--- Hotel/backend/sistema-hoteles-be/src/main/java/org/acme/model/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.acme.model;
-
-import io.quarkus.hibernate.orm.panache.PanacheRepository;
-import javax.enterprise.context.ApplicationScoped;
-
-@ApplicationScoped
-public class UserRepository implements PanacheRepository<User> {
-    // Métodos personalizados si son necesarios
-}
