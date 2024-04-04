5ee9e02a37543450a1956c5922268092c75c2c48
Rodas171315 <rodas171315@unis.edu.gt>
2024-04-04 00:29:46 -0600

    Agregando funciones para formatear fechas y horas

--- Aerolinea/frontend/aerolinea-fe/src/functions.js
+++ Aerolinea/frontend/aerolinea-fe/src/functions.js
@@ -1,20 +1,21 @@
-import Swal from 'sweetalert2'
-import { nextTick } from 'vue'
-import axios from 'axios'
+import { nextTick } from 'vue';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import moment from 'moment';
 
 export function show_alerta(msj, icon, focus) {
     if (focus !== '') {
-        nextTick(() => focus.value.focus())
+        nextTick(() => focus.value.focus());
     }
     Swal.fire({
         title: msj,
         icon: icon,
         buttonsStyling: true,
-    })
+    });
 }
 
 export function confirmation(id, url, redirect) {
-    const alert = Swal.mixin({ buttonsStyling: true })
+    const alert = Swal.mixin({ buttonsStyling: true });
     alert
         .fire({
             title: '¿Seguro de eliminar ' + id + ' ?',
@@ -25,26 +26,45 @@ export function confirmation(id, url, redirect) {
         })
         .then((result) => {
             if (result.isConfirmed) {
-                sendRequest('DELETE', {}, url, redirect)
+                sendRequest('DELETE', {}, url, redirect);
             }
-        })
+        });
 }
 
 export async function sendRequest(method, params, url, redirect = '') {
-    let res
+    let res;
     await axios({ method: method, url: url, data: params })
         .then((response) => {
-            ;(res = response.data.status),
+            (res = response.data.status),
                 show_alerta(response.data.message, 'success', ''),
-                setTimeout(() => (redirect !== '' ? (window.location.href = redirect) : ''), 2000)
+                setTimeout(() => (redirect !== '' ? (window.location.href = redirect) : ''), 2000);
         })
         .catch((errors) => {
-            let desc = ''
-            res = errors.response.data.status
+            let desc = '';
+            res = errors.response.data.status;
             errors.response.data.errors.map((e) => {
-                desc = desc + ' ' + e
-            })
-            show_alerta(desc, 'error', '')
-        })
-    return res
+                desc = desc + ' ' + e;
+            });
+            show_alerta(desc, 'error', '');
+        });
+    return res;
+}
+
+export function fechaFormateada(fecha) {
+    return moment(fecha).add(1, 'days').format('YYYY-MM-DD');
+}
+
+export function fechayhoraFormateada(fechayhora, operacion) {
+    let res;
+    if (operacion == 'create') {
+        res = fechayhora + ':00.000Z';
+        return res;
+    } else {
+        if (operacion == 'edit') {
+            res = fechayhora.substring(0, fechayhora.length - 1);
+            return res;
+        } else {
+            return moment(fechayhora).add(6, 'hours').format('YYYY-MM-DD H:mm a');
+        }
+    }
 }
